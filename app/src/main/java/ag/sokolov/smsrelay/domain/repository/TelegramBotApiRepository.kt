d57a803a4a8670950333771323562114218b9355
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-04-20 19:31:33 +0200

    Extract Telegram bot API token validation to the repository interface

--- app/src/main/java/ag/sokolov/smsrelay/domain/repository/TelegramBotApiRepository.kt
+++ app/src/main/java/ag/sokolov/smsrelay/domain/repository/TelegramBotApiRepository.kt
@@ -4,4 +4,15 @@ import ag.sokolov.smsrelay.domain.model.BotDetails
 
 interface TelegramBotApiRepository {
     suspend fun getBotDetails(botApiToken: String): Result<BotDetails>
+
+    companion object {
+        // Telegram bot API token consists of two fields, separated by a colon:
+        // 1. A numeric bot ID
+        // 2. A secret string of 35 alphanumeric characters, hyphens and underscores
+        private val botApiTokenRegex: Regex = """^\d+:[A-Za-z0-9_-]{35}$""".toRegex()
+
+        fun isTelegramBotApiTokenStructureValid(botApiToken: String): Boolean {
+            return botApiToken.matches(botApiTokenRegex)
+        }
+    }
 }
--- app/src/main/java/ag/sokolov/smsrelay/domain/use_case/set_telegram_bot_api_key/SetTelegramBotApiKeyUseCase.kt
+++ app/src/main/java/ag/sokolov/smsrelay/domain/use_case/set_telegram_bot_api_key/SetTelegramBotApiKeyUseCase.kt
@@ -1,6 +1,7 @@
 package ag.sokolov.smsrelay.domain.use_case.set_telegram_bot_api_key
 
 import ag.sokolov.smsrelay.domain.repository.TelegramBotApiRepository
+import ag.sokolov.smsrelay.domain.repository.TelegramBotApiRepository.Companion.isTelegramBotApiTokenStructureValid
 import ag.sokolov.smsrelay.domain.repository.TelegramConfigRepository
 import javax.inject.Inject
 
@@ -9,18 +10,11 @@ class SetTelegramBotApiKeyUseCase @Inject constructor(
     private val telegramBotApiRepository: TelegramBotApiRepository
 ) {
     suspend operator fun invoke(botApiToken: String): Result<Unit> {
-        if (!botApiToken.matches(botApiTokenRegex)) {
+        if (!isTelegramBotApiTokenStructureValid(botApiToken)) {
             return Result.failure(IllegalArgumentException("Invalid token format"))
         }
         return telegramBotApiRepository.getBotDetails(botApiToken).onSuccess {
-            telegramConfigRepository.setBotApiToken(botApiToken).map { }
-        }.map { }
-    }
-
-    companion object {
-        // Telegram bot API token consists of two fields, separated by a colon:
-        // 1. A numeric bot ID
-        // 2. A secret string of 35 alphanumeric characters, hyphens and underscores
-        private val botApiTokenRegex: Regex = """^\d+:[A-Za-z0-9_-]{35}$""".toRegex()
+            telegramConfigRepository.setBotApiToken(botApiToken)
+        }.map {  }
     }
 }
