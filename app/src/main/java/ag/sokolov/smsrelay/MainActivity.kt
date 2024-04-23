3c77fdb9b82dd1c52b6d8b33486646c1b8fa225c
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-04-23 02:33:53 +0200

    Clean up main activity code

--- app/src/main/java/ag/sokolov/smsrelay/MainActivity.kt
+++ app/src/main/java/ag/sokolov/smsrelay/MainActivity.kt
@@ -8,10 +8,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,27 +21,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    // Greeting("Android")
-//                    ExperimentalScreen()
-//                    TelegramBotApiTokenRegistrationExperimentalScreen()
                     TelegramBotSettingsScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!", modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SMSRelayTheme {
-        Greeting("Android")
     }
 }
