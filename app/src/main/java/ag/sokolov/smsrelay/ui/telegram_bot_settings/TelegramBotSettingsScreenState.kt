d6e4fcdd2c8dc7b2f8509d8e026658ed05530726
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-04-24 04:35:41 +0200

    Update bot settings screen state yet again

--- app/src/main/java/ag/sokolov/smsrelay/ui/telegram_bot_settings/TelegramBotSettingsScreenState.kt
+++ app/src/main/java/ag/sokolov/smsrelay/ui/telegram_bot_settings/TelegramBotSettingsScreenState.kt
@@ -1,26 +1,25 @@
 package ag.sokolov.smsrelay.ui.telegram_bot_settings
 
-sealed class TelegramBotSettingsScreenState {
+sealed class TelegramBotSettingsScreenState(
+    open val isBotAdded: Boolean,
+    open val botTitle: String,
+    open val botDescription: String
+) {
+    data object Loading : TelegramBotSettingsScreenState(
+        isBotAdded = false,
+        botTitle = "Loading...",
+        botDescription = "Requesting bot configuration"
+    )
 
-    abstract val isBotAdded: Boolean
-    abstract val botTitle: String
-    abstract val botDescription: String
-
-    data object Loading : TelegramBotSettingsScreenState() {
-        override val isBotAdded = false
-        override val botTitle = "Loading..."
-        override val botDescription = "Requesting bot configuration"
-    }
-
-    data object NotConfigured : TelegramBotSettingsScreenState() {
-        override val isBotAdded = false
-        override val botTitle = "Bot not configured"
-        override val botDescription = "Click here to set up with API token"
-    }
+    data object NotConfigured : TelegramBotSettingsScreenState(
+        isBotAdded = false,
+        botTitle = "Bot not configured",
+        botDescription = "Click here to set up with API token"
+    )
 
     data class Configured(
         override val isBotAdded: Boolean,
         override val botTitle: String,
         override val botDescription: String
-    ) : TelegramBotSettingsScreenState()
+    ) : TelegramBotSettingsScreenState(isBotAdded, botTitle, botDescription)
 }
