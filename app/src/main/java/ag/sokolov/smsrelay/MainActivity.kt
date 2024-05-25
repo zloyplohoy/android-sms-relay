7670933096e9d8e84a98f6cfd4cafb723411c1ed
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-05-25 02:37:48 +0200

    Add a TODO comment for settings viewmodel instantiation

--- app/src/main/java/ag/sokolov/smsrelay/MainActivity.kt
+++ app/src/main/java/ag/sokolov/smsrelay/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background) {
                         val navHostController = rememberNavController()
+                        // TODO: This should not be here
                         val settingsViewModel: SettingsViewModel = hiltViewModel()
                         NavHost(
                             navController = navHostController,
