9b36a3b4e72b0a34b8b9a2ff5689c3cf7655b854
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-06-02 19:30:44 +0200

    Rework main screen to use a scaffold

--- app/src/main/java/ag/sokolov/smsrelay/MainActivity.kt
+++ app/src/main/java/ag/sokolov/smsrelay/MainActivity.kt
@@ -1,40 +1,15 @@
 package ag.sokolov.smsrelay
 
-import ag.sokolov.smsrelay.ui.settings.SettingsViewModel
-import ag.sokolov.smsrelay.ui.settings.SettingsNavRoutes
-import ag.sokolov.smsrelay.ui.settings.settingsNavGraph
-import ag.sokolov.smsrelay.ui.theme.SMSRelayTheme
+import ag.sokolov.smsrelay.ui.MainScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SMSRelayTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background) {
-                        val navHostController = rememberNavController()
-                        // TODO: This should not be here
-                        val settingsViewModel: SettingsViewModel = hiltViewModel()
-                        NavHost(
-                            navController = navHostController,
-                            startDestination = SettingsNavRoutes.GRAPH_ROOT) {
-                                settingsNavGraph(navHostController, settingsViewModel)
-                            }
-                    }
-            }
-        }
+        setContent { MainScreen() }
     }
 }
---
+++ app/src/main/java/ag/sokolov/smsrelay/ui/MainScreen.kt
@@ -0,0 +1,35 @@
+package ag.sokolov.smsrelay.ui
+
+import ag.sokolov.smsrelay.ui.settings.SettingsNavRoutes
+import ag.sokolov.smsrelay.ui.settings.SettingsViewModel
+import ag.sokolov.smsrelay.ui.settings.settingsNavGraph
+import ag.sokolov.smsrelay.ui.theme.SMSRelayTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    val settingsViewModel: SettingsViewModel = hiltViewModel()
+
+    SMSRelayTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Scaffold { innerPadding ->
+                NavHost(
+                    navController = navController,
+                    startDestination = SettingsNavRoutes.GRAPH_ROOT,
+                    modifier = Modifier.padding(innerPadding)) {
+                        settingsNavGraph(navController, settingsViewModel)
+                    }
+            }
+        }
+    }
+}
