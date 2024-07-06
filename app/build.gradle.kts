f710373d259d3ef1d16aef34e95da4fb55456705
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-07-06 20:19:10 +0200

    Add compose compiler plugin for Kotlin 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
     // Dagger Hilt
     alias(libs.plugins.ksp)
     alias(libs.plugins.hilt)
