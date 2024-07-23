2a44f3c4d30c505c6ff9a2076a7caf1c0007c6fa
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-07-24 01:39:08 +0200

    Bump minSdk to 28 (Android 9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "ag.sokolov.smsrelay"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
