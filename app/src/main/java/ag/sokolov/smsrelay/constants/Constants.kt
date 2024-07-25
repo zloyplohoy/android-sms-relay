73bb250612acd61119e2abb4f7ca8d0681724885
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-07-25 03:57:54 +0200

    Add recipient verification timeout constants

--- app/src/main/java/ag/sokolov/smsrelay/constants/Constants.kt
+++ app/src/main/java/ag/sokolov/smsrelay/constants/Constants.kt
@@ -1,6 +1,11 @@
 package ag.sokolov.smsrelay.constants
 
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
 data object Constants {
+    val RECIPIENT_VERIFICATION_TIMEOUT: Duration = 10.seconds
+    val RECIPIENT_VERIFICATION_TELEGRAM_BOT_API_LONG_POLLING_TIMEOUT: Duration = 3.seconds
     const val RECIPIENT_VERIFICATION_NOTIFICATION_CHANNEL_ID: String = "RECIPIENT_VERIFICATION"
     const val RECIPIENT_VERIFICATION_NOTIFICATION_CHANNEL_NAME: String = "Recipient verification"
 }
