97643b5431319717f0dd9f36a0fb6146876a5031
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-07-25 02:42:14 +0200

    Test recipient verification worker with Hilt injection

--- app/src/main/java/ag/sokolov/smsrelay/work/RecipientVerificationWorker.kt
+++ app/src/main/java/ag/sokolov/smsrelay/work/RecipientVerificationWorker.kt
@@ -2,34 +2,41 @@ package ag.sokolov.smsrelay.work
 
 import ag.sokolov.smsrelay.R
 import ag.sokolov.smsrelay.domain.model.Constants.RECIPIENT_VERIFICATION_NOTIFICATION_CHANNEL_ID
+import ag.sokolov.smsrelay.domain.repository.ConfigurationRepository
 import android.app.Notification
 import android.content.Context
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
 import androidx.work.ForegroundInfo
-import androidx.work.Worker
 import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import java.lang.Thread.sleep
 
 // TODO: This is garbage, needs to be rewritten properly
-class RecipientVerificationWorker(
-    appContext: Context,
-    workerParams: WorkerParameters
-) : Worker(appContext, workerParams) {
-    override fun doWork(): Result {
+@HiltWorker
+class RecipientVerificationWorker @AssistedInject constructor(
+    @Assisted appContext: Context,
+    @Assisted workerParams: WorkerParameters,
+    private val configurationRepository: ConfigurationRepository
+) : CoroutineWorker(appContext, workerParams) {
+    override suspend fun doWork(): Result {
+        val token = configurationRepository.getTelegramBotApiToken()
         val pin = inputData.getString("PIN") ?: return Result.failure()
         repeat(5) {
-            if(isStopped) {
+            if (isStopped) {
                 Log.d("TAG", "doWork: Stopped $pin")
                 return Result.failure()
             }
-            Log.d("TAG", "doWork: Doing work with $pin, no shit")
+            Log.d("TAG", "doWork: Doing work with $pin, token is $token")
             sleep(5_000)
         }
         return Result.success()
     }
 
-    override fun getForegroundInfo(): ForegroundInfo {
+    override suspend fun getForegroundInfo(): ForegroundInfo {
         return ForegroundInfo(1, createNotification())
     }
 
