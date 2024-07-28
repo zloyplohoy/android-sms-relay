0230038c8fc7fc0785d6b9411d7cd23531497923
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-07-28 04:12:38 +0200

    Clean up unused imports in main app screen

--- app/src/main/java/ag/sokolov/smsrelay/ui/SMSRelayApp.kt
+++ app/src/main/java/ag/sokolov/smsrelay/ui/SMSRelayApp.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 @Composable
