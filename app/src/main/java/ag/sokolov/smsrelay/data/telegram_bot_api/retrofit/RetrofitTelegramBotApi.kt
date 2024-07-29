a1fdb151d037af30b538180937791e1d6a5319d5
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-07-29 02:58:53 +0200

    Rearrange Telegram bot API service code

--- app/src/main/java/ag/sokolov/smsrelay/data/telegram_bot_api/retrofit/RetrofitTelegramBotApi.kt
+++ app/src/main/java/ag/sokolov/smsrelay/data/telegram_bot_api/retrofit/RetrofitTelegramBotApi.kt
@@ -18,6 +18,13 @@ interface RetrofitTelegramBotApi {
         @Path("token") token: String
     ): ResponseDto<UserDto>
 
+    // https://core.telegram.org/bots/api#getchat
+    @GET("/bot{token}/getChat")
+    suspend fun getChat(
+        @Path("token") token: String,
+        @Query("chat_id") chatId: Long
+    ): ResponseDto<UserDto>
+
     // https://core.telegram.org/bots/api#getupdates
     @GET("/bot{token}/getUpdates")
     suspend fun getUpdates(
@@ -35,11 +42,4 @@ interface RetrofitTelegramBotApi {
         @Field("chat_id") chatId: Long,
         @Field("text") text: String
     ): ResponseDto<MessageDto>
-
-    // https://core.telegram.org/bots/api#getchat
-    @GET("/bot{token}/getChat")
-    suspend fun getChat(
-        @Path("token") token: String,
-        @Query("chat_id") chatId: Long
-    ): ResponseDto<UserDto>
 }
