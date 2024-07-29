4941c97e028af61e3b593005db74322b59ef0769
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-07-29 04:36:21 +0200

    Add rudimentary permission request screen

--- app/src/main/java/ag/sokolov/smsrelay/ui/setup/screen/permissions/PermissionsSetupScreen.kt
+++ app/src/main/java/ag/sokolov/smsrelay/ui/setup/screen/permissions/PermissionsSetupScreen.kt
@@ -1,12 +1,17 @@
 package ag.sokolov.smsrelay.ui.setup.screen.permissions
 
-import ag.sokolov.smsrelay.ui.setup.SetupViewModel
+import android.Manifest
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -24,9 +29,22 @@ fun NavGraphBuilder.permissionsSetupScreen(
         )
     }
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun PermissionsSetupScreen(onContinue: () -> Unit) {
-    Button(onClick = onContinue) {
-        Text(text = "Finish")
+    val permissionState = rememberPermissionState(permission = Manifest.permission.READ_SMS)
+    val requestPermissionLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) {}
+
+    Column {
+        Text(text = "Permission granted: ${permissionState.status}")
+        Button(onClick = {
+            requestPermissionLauncher.launch(Manifest.permission.READ_SMS)
+        }) {
+            Text(text = "Request permission")
+        }
+        Button(onClick = onContinue) {
+            Text(text = "Finish")
+        }
     }
 }
