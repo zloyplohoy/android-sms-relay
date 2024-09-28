83694c3ccf6badf73eee7c198e69db4e1d950591
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-09-28 06:54:06 +0200

    Apply the Android Kotlin application convention plugin to the app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.smsrelay.android.application)
     alias(libs.plugins.compose.compiler)
     // Dagger Hilt
     alias(libs.plugins.ksp)
@@ -11,12 +10,9 @@ plugins {
 
 android {
     namespace = "ag.sokolov.smsrelay"
-    compileSdk = 34
 
     defaultConfig {
         applicationId = "ag.sokolov.smsrelay"
-        minSdk = 28
-        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -34,13 +30,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
     buildFeatures {
         buildConfig = true
