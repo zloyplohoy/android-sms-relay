7c5edb7a1a54bca014847e43be19c00d73f85900
Aleksei Sokolov <sokolov.ag@sokolov.ag>
2024-09-29 02:19:12 +0200

    Switch the Telegram config repository implementation to use the Telegram config data source

--- app/src/main/java/ag/sokolov/smsrelay/data/telegram_config/TelegramConfigRepositoryImpl.kt
+++ app/src/main/java/ag/sokolov/smsrelay/data/telegram_config/TelegramConfigRepositoryImpl.kt
@@ -1,54 +1,43 @@
 package ag.sokolov.smsrelay.data.telegram_config
 
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.longPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
+import ag.sokolov.smsrelay.core.datastore.TelegramConfigDataSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class TelegramConfigRepositoryImpl @Inject constructor(
-    private val dataStore: DataStore<Preferences>
+    private val telegramConfigDataSource: TelegramConfigDataSource
 ) : TelegramConfigRepository {
-
-    companion object {
-        private val TOKEN = stringPreferencesKey("telegram_config_bot_api_token")
-        private val RECIPIENT_ID = longPreferencesKey("telegram_config_recipient_id")
-    }
 
     // Telegram bot API token
 
     override suspend fun getToken(): String? =
-        dataStore.data.first()[TOKEN]
+        telegramConfigDataSource.botApiToken.first()
 
     override fun getTokenFlow(): Flow<String?> =
-        dataStore.data.map { it[TOKEN] }.distinctUntilChanged()
+        telegramConfigDataSource.botApiToken
 
     override suspend fun setToken(token: String) {
-        dataStore.edit { it[TOKEN] = token }
+        telegramConfigDataSource.setBotApiToken(token)
     }
 
     override suspend fun deleteToken() {
-        dataStore.edit { it.remove(TOKEN) }
+        telegramConfigDataSource.deleteBotApiToken()
     }
 
     // Telegram recipient ID
 
     override suspend fun getRecipientId(): Long? =
-        dataStore.data.first()[RECIPIENT_ID]
+        telegramConfigDataSource.recipientId.first()
 
     override fun getRecipientIdFlow(): Flow<Long?> =
-        dataStore.data.map { it[RECIPIENT_ID] }.distinctUntilChanged()
+        telegramConfigDataSource.recipientId
 
     override suspend fun setRecipientId(recipientId: Long) {
-        dataStore.edit { it[RECIPIENT_ID] = recipientId }
+        telegramConfigDataSource.setRecipientId(recipientId)
     }
 
     override suspend fun deleteRecipientId() {
-        dataStore.edit { it.remove(RECIPIENT_ID) }
+        telegramConfigDataSource.deleteRecipientId()
     }
 }
